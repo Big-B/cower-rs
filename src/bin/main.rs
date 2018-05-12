8f5f85d7ee723728c4ce4d982c4c53c9ee0f7844
Brandon Mittman <BrandonMittman@gmail.com>
2018-05-11 18:41:47 -0700

    Move some things around, bump minor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate clap;
 extern crate stderrlog;
-extern crate ferris_says;
 extern crate log;
 
 extern crate cower_rs;
@@ -9,12 +8,10 @@ extern crate cower_rs;
 use clap::{Arg, ArgGroup, App};
 use std::path::PathBuf;
 use std::{env, str};
-use cower_rs::config::{Config, OpMask, SearchBy, SortOrder};
+use cower_rs::*;
+use cower_rs::config::*;
 use std::error::Error;
 use log::Level;
-use std::io::BufWriter;
-use std::string::FromUtf8Error;
-use ferris_says::say;
 
 fn main() {
     let mut config = Config::new();
@@ -54,36 +51,6 @@ pub fn get_config_path() -> Option<PathBuf> {
     } else {
         None
     }
-}
-
-fn get_version_string() -> Result<String, FromUtf8Error> {
-    // Make cower cow
-    let mut version = String::new();
-    version.push_str("\n  ");
-    version.push_str(crate_version!());
-    version.push_str("\n");
-    version.push_str("     \\\n");
-    version.push_str("      \\\n");
-    version.push_str("        ,__, |    |\n");
-    version.push_str("        (oo)\\|    |___\n");
-    version.push_str("        (__)\\|    |   )\\_\n");
-    version.push_str("          U  |    |_w |  \\\n");
-    version.push_str("             |    |  ||   *\n");
-    version.push_str("\n");
-    version.push_str("             Cower-rs.\n\n");
-
-    // Get max line size
-    let len = version.split('\n').map(|s| s.len()).max().unwrap_or(30);
-
-    // Make a buf writer for ferris_says
-    let mut buf = BufWriter::new(Vec::new());
-    say(&version.into_bytes(), len, &mut buf).unwrap();
-
-    // Prefix output with a newline to fix clap not using one
-    let mut fin = String::new();
-    fin.push_str("\n");
-    fin.push_str(String::from_utf8(buf.into_inner().unwrap())?.as_str());
-    Ok(fin)
 }
 
 /// Handle the command line arguments
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,40 @@ extern crate bitflags;
 extern crate isatty;
 extern crate log;
 extern crate time;
+extern crate ferris_says;
 
 pub mod config;
+
+use std::io::BufWriter;
+use std::string::FromUtf8Error;
+use ferris_says::say;
+
+pub fn get_version_string() -> Result<String, FromUtf8Error> {
+    // Make cower cow
+    let mut version = String::new();
+    version.push_str("\n  ");
+    version.push_str(env!("CARGO_PKG_VERSION"));
+    version.push_str("\n");
+    version.push_str("     \\\n");
+    version.push_str("      \\\n");
+    version.push_str("        ,__, |    |\n");
+    version.push_str("        (oo)\\|    |___\n");
+    version.push_str("        (__)\\|    |   )\\_\n");
+    version.push_str("          U  |    |_w |  \\\n");
+    version.push_str("             |    |  ||   *\n");
+    version.push_str("\n");
+    version.push_str("             Cower-rs.\n\n");
+
+    // Get max line size
+    let len = version.split('\n').map(|s| s.len()).max().unwrap_or(30);
+
+    // Make a buf writer for ferris_says
+    let mut buf = BufWriter::new(Vec::new());
+    say(&version.into_bytes(), len, &mut buf).unwrap();
+
+    // Prefix output with a newline to fix clap not using one
+    let mut fin = String::new();
+    fin.push_str("\n");
+    fin.push_str(String::from_utf8(buf.into_inner().unwrap())?.as_str());
+    Ok(fin)
+}
